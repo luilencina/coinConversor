aa9c3a31b2bb65e86fcf65ee80470ea0f653451d
luilencina <luizalencina@hotmail.com>
2025-07-13 14:29:51 -0300

    dark mode and zone.js

--- coin-conversion/src/main.ts
+++ coin-conversion/src/main.ts
@@ -1,10 +1,11 @@
+import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { App } from './app/app';
-import { importProvidersFrom } from '@angular/core';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(App, {
   providers: [
-    importProvidersFrom(
-    ),
-  ],
+    provideRouter(routes),
+  ]
 });
